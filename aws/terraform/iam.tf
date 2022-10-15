7294b605982e1f80153ea0421edf55bba4cff195
koki-develop <kou.pg.0131@gmail.com>
2022-10-15 15:34:36 +0900

    github actions 用の IAM ロールを作成

--- aws/terraform/iam.tf
+++ aws/terraform/iam.tf
@@ -13,3 +13,29 @@ resource "aws_iam_openid_connect_provider" "github_actions" {
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = data.tls_certificate.github_actions.certificates[*].sha1_fingerprint
 }
+
+data "aws_iam_policy_document" "example_assume_role_policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github_actions.arn]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:koki-develop/gh-actions-oidc-example:*"]
+    }
+  }
+}
+
+resource "aws_iam_role" "example" {
+  name               = "oidc-example-role"
+  assume_role_policy = data.aws_iam_policy_document.example_assume_role_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "example_s3_readonly" {
+  role       = aws_iam_role.example.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
